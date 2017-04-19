eab1b1216defa7f22a25400d3255d48cda8a83ab
Sean Felling <Snfelling7@gmail.com>
2017-04-19 08:47:21 -0500

    Finishedgit add .

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ function onReady() {
   // when response comes back from the server
   // run success function which will log response
   $.ajax({
-    url: 'http://www.omdbapi.com/?t=jaws',
+    url: 'http://www.omdbapi.com/?t=freddy+vs+jason',
     success: function(response) {
       console.log('this is the response ->', response);
       console.log('movie title ->', response.Title);
       // console.log('movie actors ->', response.Actors);
       // put title on the DOM
-      $('#movie-holder').append('<div class="movie-title">' + response.Title + '</div>');
+      $('#context').append('<div class="movie-title">' + response.Title + '</div>');
+      $('#movie-holder').append( '<div class="movie-holder">' + '<img src="'+ response.Poster + '">');
     }
     // error: function(response) {
     //   console.log('error case: response ->', response);
